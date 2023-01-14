dbdd363d8b422316a20536860c30512a303c4c3c
wangzihao <1736264610@qq.com>
2023-01-14 20:53:31 +0800

    macOs修正

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,5 +3,6 @@ public class Helloworld {
         System.out.println("Helloworld!!!");
         System.out.println("this is a text to match this code to github");
         int[] a = new int[5];
+        System.out.println(a);
     }
 }
