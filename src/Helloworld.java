11371cabc8065beee4f253b81d7a600548ef369d
wangzihao <1736264610@qq.com>
2023-01-14 20:57:47 +0800

    macOs修正

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Helloworld!!!");
         System.out.println("this is a text to match this code to github");
         int[] a = new int[5];
-        System.out.println(a);
+        System.out.println(Arrays.toString(a));
     }
 }
